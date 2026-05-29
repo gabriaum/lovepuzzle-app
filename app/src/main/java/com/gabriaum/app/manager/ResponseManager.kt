2b17b9539963c4b7e66640b82fe162826e4a8bb9
gabriaum <gaabrielbruck@gmail.com>
2026-05-29 16:54:53 -0300

    refactor: change ResponseManager to use a private list for responses and add access methods

--- app/src/main/java/com/gabriaum/app/manager/ResponseManager.kt
+++ app/src/main/java/com/gabriaum/app/manager/ResponseManager.kt
@@ -2,13 +2,27 @@ package com.gabriaum.app.manager
 
 import com.gabriaum.app.model.Response
 
-class ResponseManager: ArrayList<Response>() {
-    init {
-        add(Response("Question 1", "yes"))
-        add(Response("Question 2", "yes"))
-        add(Response("Question 3", "yes"))
-        add(Response("Question 4", "yes"))
-        add(Response("Question 5", "yes"))
-        add(Response("Question 6 ", "yes"))
+class ResponseManager {
+    private val responses: MutableList<Response> = mutableListOf(
+        Response("Question 1", "yes"),
+        Response("Question 2", "yes"),
+        Response("Question 3", "yes"),
+        Response("Question 4", "yes"),
+        Response("Question 5", "yes"),
+        Response("Question 6 ", "yes")
+    )
+
+    fun getResponseCount(): Int = responses.size
+
+    fun getResponse(index: Int): Response {
+        if (index < 0 || index >= responses.size) {
+            throw IndexOutOfBoundsException("Invalid response index: $index")
+        }
+        return responses[index]
     }
+
+    operator fun get(index: Int): Response = getResponse(index)
+
+    val size: Int
+        get() = responses.size
 }
